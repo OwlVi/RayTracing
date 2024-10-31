175d92b41f7657a31cacd2863528799f625f0d22
OwlVi <mrowlvi@gmail.com>
2024-11-01 00:47:55 +0700

    Setup

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ import RT_integrator as rti
 import RT_light as rtl
 import RT_texture as rtt
 
-def renderDoF():
+pixel = 1920
+spp = 100
+mdepth = 5
+
+def renderScene1():
     main_camera = rtc.Camera()
     main_camera.aspect_ratio = 16.0/9.0
-    main_camera.img_width = 480
+    main_camera.img_width = pixel
     main_camera.center = rtu.Vec3(0,0,0)
-    main_camera.samples_per_pixel = 100
-    main_camera.max_depth = 5
+    main_camera.samples_per_pixel = spp
+    main_camera.max_depth = mdepth
     main_camera.vertical_fov = 60
     main_camera.look_from = rtu.Vec3(-2, 2, 1)
     main_camera.look_at = rtu.Vec3(0, 0, -1)
@@ -24,10 +28,9 @@ def renderDoF():
     focus_distance = 5.0
     aperture = 1.0
     main_camera.init_camera(defocus_angle, focus_distance, aperture)
-    # add objects to the scene
-
+    
+    # Setting Material
     tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
-
     mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
 
     mat_blinn1 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.8), 0.5, 0.2, 8)
@@ -35,6 +38,7 @@ def renderDoF():
     mat_blinn3 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.4), 0.5, 0.2, 8)
 
 
+    # add objects to the scene
     world = rts.Scene()
     world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_checker_bw))
     world.add_object(rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_blinn1))    # left
@@ -44,18 +48,17 @@ def renderDoF():
     intg = rti.Integrator(bSkyBG=True)
 
     renderer = rtren.Renderer(main_camera, intg, world)
-    #๒renderer.render_jittered_aperture()
-    #.write_img2png('week10_jitter_DoF.png')    
-    renderer.render_aperture()
-    renderer.write_img2png('week10_DoF.png')    
 
-def renderMoving():
+    renderer.render()
+    renderer.write_img2png('Scene-1.png')    
+
+def renderScene2():
     main_camera = rtc.Camera()
     main_camera.aspect_ratio = 16.0/9.0
-    main_camera.img_width = 480
+    main_camera.img_width = pixel
     main_camera.center = rtu.Vec3(0,0,0)
-    main_camera.samples_per_pixel = 100
-    main_camera.max_depth = 5
+    main_camera.samples_per_pixel = spp
+    main_camera.max_depth = mdepth
     main_camera.vertical_fov = 60
     main_camera.look_from = rtu.Vec3(-2, 2, 1)
     main_camera.look_at = rtu.Vec3(0, 0, -1)
@@ -65,10 +68,9 @@ def renderMoving():
     focus_distance = 5.0
     aperture = 1.0
     main_camera.init_camera(defocus_angle, focus_distance,aperture)
-    # add objects to the scene
 
+    # Setting Material
     tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
-
     mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
 
     mat_blinn1 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.8), 0.5, 0.2, 8)
@@ -76,6 +78,7 @@ def renderMoving():
     mat_blinn3 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.4), 0.5, 0.2, 8)
 
 
+    # add objects to the scene
     sph_left = rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_blinn1)
     sph_left.add_moving(rtu.Vec3(-1.0,   0.0,-1) + rtu.Vec3(0.0, 0.5,0.0))
 
@@ -89,10 +92,11 @@ def renderMoving():
 
     renderer = rtren.Renderer(main_camera, intg, world)
     renderer.render_jittered()
-    renderer.write_img2png('week10_moving_nojitter.png')    
+    renderer.write_img2png('Scene2.png')    
 
 if __name__ == "__main__":
-    # renderDoF()
-    renderMoving()
+    
+    renderScene1()
+    #renderScene2()
 
 
