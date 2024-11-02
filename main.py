64b1fd65bdf846a2c473974cd3b276e5effcad96
OwlVi <mrowlvi@gmail.com>
2024-11-03 03:57:09 +0700

    Set to render

--- main.py
+++ main.py
@@ -181,48 +181,62 @@ def renderScene1():
 
     renderer = rtren.Renderer(main_camera, intg, world)
 
-    renderer.render()
+    renderer.render_jittered()
     renderer.write_img2png('Scene-1.png')    
 
 def renderScene2():
     main_camera = rtc.Camera()
     main_camera.aspect_ratio = 16.0/9.0
-    main_camera.img_width = 320
+    main_camera.img_width = pixel
     main_camera.center = rtu.Vec3(0,0,0)
-    main_camera.samples_per_pixel = 5
-    main_camera.max_depth = mdepth
+    main_camera.samples_per_pixel = spp
+    main_camera.max_depth = 5
     main_camera.vertical_fov = 60
-    main_camera.look_from = rtu.Vec3(-2, 2, 1)
+    main_camera.look_from = rtu.Vec3(0, 2, 2)# (x rotation ,y rotation,z rotation)
     main_camera.look_at = rtu.Vec3(0, 0, -1)
     main_camera.vec_up = rtu.Vec3(0, 1, 0)
 
-    defocus_angle = 0.0
+    defocus_angle = 2.0
     focus_distance = 5.0
     aperture = 1.0
-    main_camera.init_camera(defocus_angle, focus_distance,aperture)
-
+    main_camera.init_camera(defocus_angle, focus_distance, aperture)
+    
     # Setting Material
-    tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
-    mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
-
-    mat_blinn1 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.8), 0.5, 0.2, 8)
-    mat_blinn2 = rtm.Blinn(rtu.Color(0.4, 0.5, 0.4), 0.5, 0.6, 8)
-    mat_blinn3 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.4), 0.5, 0.2, 8)
-
+    ground = rtm.Dielectric(rtu.Color(0.270, 0.667, 0.9),3.33)
+    red_mat = rtm.Blinn(rtu.Color(1,0,0),   0.8, 14,   2)
+    blue_mat = rtm.Blinn( rtu.Color(0,0,1), 0.8, 14,   2)
+    point_light = rtl.Diffuse_light(rtu.Color(0.125,0.125,0.125))
 
     # add objects to the scene
-    sph_left = rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_blinn1)
-    sph_left.add_moving(rtu.Vec3(-1.0,   0.0,-1) + rtu.Vec3(0.0, 0.5,0.0))
-
     world = rts.Scene()
-    world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_checker_bw))
-    world.add_object(sph_left)    # left
-    world.add_object(rto.Sphere(rtu.Vec3(   0,   0.0,-1),  0.5, mat_blinn2))    # center
-    world.add_object(rto.Sphere(rtu.Vec3( 1.0,   0.0,-1),  0.5, mat_blinn3))    # right
-
+    # background
+    world.add_object(rto.Sphere(rtu.Vec3(   0,-101.5,-1),  100,ground))
+    
+    # Light
+    point = rto.Sphere(rtu.Vec3(0,5,3),1,point_light)
+    world.add_object(point)
+    
+    # Sphere
+    r_sphere = 0.75
+    # sphere left 
+    left_centerP = rtu.Vec3(-0.1,1,0)
+    sphere_left = rto.Sphere(left_centerP,r_sphere,red_mat)
+    sphere_left.add_moving(left_centerP + rtu.Vec3(.5,0,0))
+    world.add_object(sphere_left)
+    
+    # sphere right 
+    right_centerP =rtu.Vec3(0.1,1,0)
+    sphere_right = rto.Sphere(right_centerP,r_sphere,blue_mat)
+    sphere_right.add_moving(right_centerP + rtu.Vec3(-.5,0,0))
+    world.add_object(sphere_right)
+    
+    
+    
     intg = rti.Integrator(bSkyBG=True)
 
     renderer = rtren.Renderer(main_camera, intg, world)
+
+    renderer.render()
     renderer.render_jittered()
     renderer.write_img2png('Scene2.png')    
 
