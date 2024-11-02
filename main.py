6b9087ad992bc37f5e688d591c8a8ab2ca6e2f2b
OwlVi <mrowlvi@gmail.com>
2024-11-02 21:05:42 +0700

    RenderScene1

--- main.py
+++ main.py
@@ -136,23 +136,81 @@ def renderScene1():
     aperture = 1.0
     main_camera.init_camera(defocus_angle, focus_distance, aperture)
     
-    # Setting Material
-    tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
-    mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
-
-    mat_blinn1 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.8), 0.5, 0.2, 8)
-    mat_blinn2 = rtm.Blinn(rtu.Color(0.4, 0.5, 0.4), 0.5, 0.6, 8)
-    mat_blinn3 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.4), 0.5, 0.2, 8)
-
+   # Setting Material
+    
+    light_red = rtl.SaberLight(rtu.Color(1,0.15,0.15))
+    light_greed = rtl.SaberLight(rtu.Color(0.15,1,0.15))
+    light_white = rtl.Diffuse_light(rtu.Color(1,1,1))
+    metal_mat = rtm.Metal(rtu.Color(0.612,0.612,0.612),0.005)
 
     # add objects to the scene
     world = rts.Scene()
-    world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_checker_bw))
-    world.add_object(rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_blinn1))    # left
-    world.add_object(rto.Sphere(rtu.Vec3(   0,   0.0,-1),  0.5, mat_blinn2))    # center
-    world.add_object(rto.Sphere(rtu.Vec3( 1.0,   0.0,-1),  0.5, mat_blinn3))    # right
 
-    intg = rti.Integrator(bSkyBG=True)
+    # vCenter light same base
+    # lightsaber lEFT
+    obj_center_left = rtu.Vec3( -1.5, -1, -0.75)
+    obj_axit_left = rtu.Vec3(1,1,0)
+
+    # lightsaber RIGHT
+    obj_center_right = rtu.Vec3( 1.5, -1, -0.5)
+    obj_axit_right = rtu.Vec3(-1,1,0)
+   
+    # ---------------------
+    r_bullet = 0.04
+    e_bullet = 1.4
+    # Bullet 1
+    x1 = 1
+    y1 = -1
+    z1 = 0.2
+    bulletS_1 = rtu.Vec3(x1, y1, z1)
+    bulletE_1 = rtu.Vec3(x1+e_bullet, y1, z1)
+    bullet1 = rto.Capsule(bulletS_1,  bulletE_1, r_bullet,light_white)
+    bullet1.add_moving(rtu.Vec3(-0.1,0,0))
+    # Bullet 2
+    x2 = -2
+    y2 = -1
+    z2 = -2
+    bulletS_2 = rtu.Vec3( x2, y2, z2)
+    bulletE_2 = rtu.Vec3(x2-e_bullet, y2, z2)
+    bullet2 = rto.Capsule(bulletS_2,bulletE_2, r_bullet,light_white)
+    bullet2.add_moving(rtu.Vec3(0.1,0,0))
+    # Bullet 3
+    x3 = 0.6
+    y3 = 0
+    z3 = -0.6
+    bulletS_3 = rtu.Vec3( x3, y3, z3)
+    bulletE_3 = rtu.Vec3(x3-e_bullet, y3, z3)
+    bullet3 = rto.Capsule(bulletS_3,bulletE_3, r_bullet,light_white)
+    bullet3.add_moving(rtu.Vec3(0.1,0,0))
+    # Bullet 4
+    x4 = -1.5
+    y4 = 1
+    z4 = -0.25
+    bulletS_4 = rtu.Vec3( x4, y4, z4)
+    bulletE_4 = rtu.Vec3(x4-e_bullet, y4, z4)
+    bullet4 = rto.Capsule(bulletS_4,bulletE_4, r_bullet,light_white)
+    bullet4.add_moving(rtu.Vec3(0.1,0,0))
+    # Bullet 5
+    x5 = 1
+    y5 = 0.75
+    z5 = -0.5
+    bulletS_5 = rtu.Vec3( x5, y5, z5)
+    bulletE_5 = rtu.Vec3(x5+e_bullet, y5, z5)
+    bullet5 = rto.Capsule(bulletS_5,bulletE_5, r_bullet,light_white)
+    bullet5.add_moving(rtu.Vec3(-0.1,0,0))
+    
+    # Add to world
+    world.add_object(rto.Cylinder(obj_center_left,  0.08, 3.4, obj_axit_left,light_red))   # light
+    world.add_object(rto.Cylinder(obj_center_left,  0.14, 0.8, obj_axit_left,metal_mat))    # base    
+    world.add_object(rto.Cylinder(obj_center_right,  0.08, 3.4, obj_axit_right,light_greed))    # light
+    world.add_object(rto.Cylinder(obj_center_right,  0.14, 0.8, obj_axit_right,metal_mat))    # base
+    world.add_object(bullet1)# bullet    
+    world.add_object(bullet2)# bullet    
+    world.add_object(bullet3)# bullet
+    world.add_object(bullet4)# bullet
+    world.add_object(bullet5)# bullet
+
+    intg = rti.Integrator(bSkyBG=False)
 
     renderer = rtren.Renderer(main_camera, intg, world)
 
@@ -202,8 +260,7 @@ def renderScene2():
     renderer.write_img2png('Scene2.png')    
 
 if __name__ == "__main__":
-    renderTest()
-    #renderScene1()
+    renderScene1()
     #renderScene2()
 
 
